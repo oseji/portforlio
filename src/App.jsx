ba8b359f71cef248752d6c58acfa4433579cf163
oseji <jiade1233@gmail.com>
2024-01-30 17:37:04 +0100

    changed jpgs to pngs and implemented logic to show all or less projects

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,11 @@ function App() {
 
   const [isThemeToggled, setIsThemeToggled] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const [areProjectsFiltered, setAreProjectsFiltered] = useState(true);
+  const filteredProjects = projectData.slice(0, 3);
+  const [dataToMap, setDataToMap] = useState(filteredProjects);
+
   const [text, setText] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
   const words = ["", " , a frontend dev"];
@@ -160,6 +165,13 @@ function App() {
 
     setText(currentText);
   }, [currentIndex, text]);
+
+  //toggling between showing all projects and showing just first 3
+  useEffect(() => {
+    areProjectsFiltered
+      ? setDataToMap(filteredProjects)
+      : setDataToMap(projectData);
+  }, [areProjectsFiltered]);
 
   //LOGIC TO CHECK CURRENT DEVICE THEME
   // useEffect(() => {
@@ -419,66 +431,79 @@ function App() {
           <h1 className="sectionHeading col-span-full">
             <span className="headingNum">02. </span>PROJECTS
           </h1>
-          {projectData.map((element, index) => (
-            <motion.div
-              key={index}
-              className="projectContainer"
-              variants={projectContainerVariants}
-              initial="hidden"
-              whileInView="visible"
-            >
-              <img src={element.img} alt={element.alt} className="projectImg" />
-
-              <div
-                className={`projectText ${
-                  isThemeToggled
-                    ? "projectContainerDark"
-                    : "projectContainerLight"
-                }`}
+          <div className="displayProjects">
+            {dataToMap.map((element, index) => (
+              <motion.div
+                key={index}
+                className="projectContainer"
+                variants={projectContainerVariants}
+                initial="hidden"
+                whileInView="visible"
               >
-                <div>
-                  <h1 className="projectName">{element.title}</h1>
+                <img
+                  src={element.img}
+                  alt={element.alt}
+                  className="projectImg"
+                />
 
-                  <p className="aboutProject">{element.about}</p>
-                </div>
+                <div
+                  className={`projectText ${
+                    isThemeToggled
+                      ? "projectContainerDark"
+                      : "projectContainerLight"
+                  }`}
+                >
+                  <div>
+                    <h1 className="projectName">{element.title}</h1>
 
-                <div>
-                  <div className="stackGrp">
-                    {element.stack.map((stack, index) => (
-                      <p
-                        className={`stack ${
-                          isThemeToggled ? "stackDark" : "stackLight"
-                        }`}
-                        key={index}
-                      >
-                        {stack}
-                      </p>
-                    ))}
+                    <p className="aboutProject">{element.about}</p>
                   </div>
 
-                  <div className="iconGrp">
-                    <a href={element.githubRepo} target="_blank">
-                      {iconGithub}
-                    </a>
+                  <div>
+                    <div className="stackGrp">
+                      {element.stack.map((stack, index) => (
+                        <p
+                          className={`stack ${
+                            isThemeToggled ? "stackDark" : "stackLight"
+                          }`}
+                          key={index}
+                        >
+                          {stack}
+                        </p>
+                      ))}
+                    </div>
 
-                    <div className=" flex flex-row items-center gap-2 hover:text-orange-400 duration-100">
-                      <a
-                        href={element.href}
-                        target="_blank"
-                        className={`font-semibold  text-sm  ${
-                          isThemeToggled ? `border-white` : `border-black`
-                        }`}
-                      >
-                        Live site
+                    <div className="iconGrp">
+                      <a href={element.githubRepo} target="_blank">
+                        {iconGithub}
                       </a>
 
-                      {linkSVG}
+                      <div className=" flex flex-row items-center gap-2 hover:text-orange-400 duration-100">
+                        <a
+                          href={element.href}
+                          target="_blank"
+                          className={`font-semibold  text-sm  ${
+                            isThemeToggled ? `border-white` : `border-black`
+                          }`}
+                        >
+                          Live site
+                        </a>
+
+                        {linkSVG}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </motion.div>
-          ))}
+              </motion.div>
+            ))}
+          </div>
+
+          <button
+            className="block mx-auto font-semibold mt-5 hover:underline"
+            onClick={() => setAreProjectsFiltered(!areProjectsFiltered)}
+          >
+            {areProjectsFiltered ? "Show All" : "Show less"}
+          </button>
         </motion.section>
 
         <section id="contactMe">
