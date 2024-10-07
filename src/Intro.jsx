d1783798a4fec6bca1a99b8af9a9746ec8a13b9e
oseji <jiade1233@gmail.com>
2024-10-07 15:34:57 +0100

    changed animation a bit

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -28,12 +28,12 @@ const Intro = () => {
       text1.chars,
       {
         opacity: 0,
-        y: -50,
+        x: 100,
         scale: 0.1,
       },
       {
         opacity: 1,
-        y: 0,
+        x: 0,
         scale: 1,
         stagger: 0.07,
       }
@@ -42,12 +42,12 @@ const Intro = () => {
         text2.chars,
         {
           opacity: 0,
-          y: -50,
+          x: 100,
           scale: 0.1,
         },
         {
           opacity: 1,
-          y: 0,
+          x: 0,
           scale: 1,
           stagger: 0.07,
         }
@@ -56,12 +56,12 @@ const Intro = () => {
         text3.chars,
         {
           opacity: 0,
-          y: -50,
+          x: 100,
           scale: 0.1,
         },
         {
           opacity: 1,
-          y: 0,
+          x: 0,
           scale: 1,
           stagger: 0.07,
         }
